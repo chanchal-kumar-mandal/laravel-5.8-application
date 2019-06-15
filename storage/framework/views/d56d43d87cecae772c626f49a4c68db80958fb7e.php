beb704f9f3267d65cd8924052b732f00cef97266
Chanchal <ckmandal9@gmail.com>
2019-06-15 12:53:41 +0530

    CRUD operation in laravel 5.8

---
+++ storage/framework/views/d56d43d87cecae772c626f49a4c68db80958fb7e.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 5.8 CRUD Application</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+  
+<div class="container">
+    <?php echo $__env->yieldContent('content'); ?>
+</div>
+   
+</body>
+</html><?php /**PATH C:\xampp\htdocs\laravel-user-application\resources\views/products/layout.blade.php ENDPATH**/ ?>
